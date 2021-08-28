239782867fd148ae46b05a0eba5f79145f3168f6
Dev_coder <emmanuelomoiya6@gmail.com>
2021-08-28 12:43:24 +0100

    make it better

---
+++ public/js/api.js
@@ -0,0 +1,11 @@
+const api_key = "7b03ae7cf7502f31371d10661a185f8b";
+
+const img_url = "https://image.tmdb.org/t/p/w500/";
+
+const original_img_url = "https://image.tmdb.org/t/p/original";
+
+const genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+const movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+
+const movie_detail_http = "https://api.themoviedb.org/3/movie/"
